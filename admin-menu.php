79878d9ef10466753b254fd1d12419ed21b1d446
RIdwanHasanah <sun.riseagain@yahoo.com>
2016-12-28 14:41:41 +0700

    penambahan option page

--- admin-menu.php
+++ admin-menu.php
@@ -1,21 +1,26 @@
 <?php
 
-add_action('admin_menu', 'create_paypalku_options_page');
-add_action('admin_init', 'register_and_build_fields');
-/*admin_init di gunakan untuk mengakses admin area 
-register_and_build_fields adalah isi dari fungsi akses*/
-
 function create_paypalku_options_page() {
    add_options_page('PaypalKu', 'PaypalKu', 'administrator', __FILE__, 'options_page_fn');
 }
 
+add_action('admin_menu', 'create_paypalku_options_page');
+
+
 function register_and_build_fields() {
-   register_setting('plugin_options', 'plugin_options', 'validate_setting');
+   register_setting('plugin_options', 'plugin_options');
 
    add_settings_section('main_section', 'Main Settings', 'section_cb', __FILE__);
 
-   add_settings_field('email_paypalku', 'Email Paypal : ', 'email_paypalku_setting', __FILE__, 'main_section');
+   add_settings_field('email_paypalku', 'Email Paypal ', 'email_paypalku_setting', __FILE__, 'main_section');
+   add_settings_field('harga_dollar', 'Dollar', 'harga_dollar_setting', __FILE__, 'main_section');
+   add_settings_field('currency_code', 'Currency', 'currency_code_setting', __FILE__, 'main_section');
 }
+
+add_action('admin_init', 'register_and_build_fields');
+/*admin_init di gunakan untuk mengakses admin area 
+register_and_build_fields adalah isi dari fungsi akses*/
+
 
 function options_page_fn() {
 ?>
@@ -39,16 +44,38 @@ function options_page_fn() {
 // Email Paypal
 function email_paypalku_setting() {
    $options = get_option('plugin_options');
-   echo "<input type='text' name='plugin_options['email_paypalku'] value='{$options['email_paypalku']}'/>";
+   echo "<input type='text' name='plugin_options[email_paypalku]' value='{$options['email_paypalku']}'/>
+   contoh test.@gmail.com ";
+}
+//Harga Dollar
+function harga_dollar_setting(){
+   $options = get_option('plugin_options');
+   echo "<input type='number' name='plugin_options[harga_dollar]' value='{$options['harga_dollar']}' />
+   contoh : 13333 jangan menggunakan tanda koma ( , )";
+
+}
+//Currency
+function currency_code_setting(){
+   $options = get_option('plugin_options');
+
+   ?>
+   <select name='plugin_options[currency_code]'>
+      <option value='1' <?php selected( $options['currency_code'], 1 ); ?>>USD</option>
+      <option value='2' <?php selected( $options['currency_code'], 2 ); ?>>EUR</option>
+   </select>
+
+<?php
+   /*echo "<input type='text' maxlength='3' name='plugin_options[currency_code]' value='{$options['currency_code']}' />
+   contoh : USD, EUR dll.";*/
 }
 
 
-function validate_setting($plugin_options) {
+/*function validate_setting($plugin_options) {
    $keys = array_keys($_FILES);
    $i = 0;
 
    return $plugin_options;
-}
+}*/
 
 function section_cb() {}
 
@@ -59,3 +86,5 @@ function admin_register_head() {
    $url = get_bloginfo('template_directory') . '/functions/options_page.css';
    echo "<link rel='stylesheet' href='$url' />\n";
 }
+
+
--- button_paypal.php
+++ button_paypal.php
@@ -1,9 +1,52 @@
 <?php
 
+/*Ini akan tampil di PostContent*/
+function rh_paypal() { 
+  // email_paypalku_setting();
+   ob_start();
+   $options = get_option('plugin_options');
+
+   
+   ?>
+
+   <form name="input" method="post" action="#">
+      <br>
+      Harga Rp  <input type="number" name="harga"  id="harga" value="<?= $harga = $_POST['harga']; ?>">
+      <br>
+      <input type="submit" name="submit_item">
+   </form>
+   <?php 
+   
+   if (isset($_POST['submit_item'] ) ) {
+       $dollar = $options['harga_dollar'];
+       $harga = $_POST['harga'];
+
+       $harga2 = $harga/$dollar;
+      ?>
+      <form name="_xclick" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_BLANK">
+         <input type="hidden" name="cmd" value="_xclick">
+         <input type="hidden" name="business" value="<?= $options['email_paypalku']; ?>">
+         <input type="hidden" name="currency_code" value="<?= strtoupper($options['plugin_options[currency_code]']); ?>">
+         <input type="hidden" name="item_name" value="<?php the_title(); ?>">
+         <input type="hidden" name="amount" value="<?= $harga2; ?>">
+         <input src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" name="Submit" type="image" value="pruchase" alt="Purchase" />
+      </form>
+
+      <?php
+   } elseif (isset($_POST['nm_item']) && $_POST['harga'] == '') {
+      echo 'Silahkan isi semua field';
+   }
+
+return ob_get_clean();
+
+}
+
+add_shortcode('paypal', 'rh_paypal');
+
 
 
 /*Ini akan tampil di PostContent*/
-function rh_paypal() { 
+/*function rh_paypal() { 
 	ob_start(); ?>
 
 	<form name="input" method="post" action="#">
@@ -21,7 +64,7 @@ function rh_paypal() {
 		?>
 		<form name="_xclick" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_BLANK">
 			<input type="hidden" name="cmd" value="_xclick">
-			<input type="hidden" name="business" value="<?= $options; ?>">
+			<input type="hidden" name="business" value="{$options['email_paypalku']}">
 			<input type="hidden" name="currency_code" value="USD">
 			<input type="hidden" name="item_name" value="<?php echo $item; ?>">
 			<input type="hidden" name="amount" value="<?= $harga; ?>">
@@ -29,6 +72,7 @@ function rh_paypal() {
 		</form>
 
 		<?php
+		//echo $options;
 	} elseif (isset($_POST['nm_item']) && $_POST['harga'] == '') {
 		echo 'Silahkan isi semua field';
 		echo $options;
@@ -38,6 +82,6 @@ function rh_paypal() {
 
 }
 
-add_shortcode('paypal', 'rh_paypal');
+add_filter('the_content', 'rh_paypal');*/
 
 ?>
